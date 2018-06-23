ebde1cd6f21292b3d7d04edf37d62aa512d94851
Ryan Craig Martin <rmart106@fiu.edu>
2018-06-23 18:44:31 -0400

    FEAT: State updates with add to order button.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import Fish from "./Fish"; 
 import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
@@ -26,11 +27,24 @@ class App extends React.Component {
     })
   }
 
+  addToOrder = (key) => {
+    // 1. Take a copy of the state.
+    const order = {...this.state.order}
+    // 2. Either add to the order or update the number in our order.
+    order[key] = order[key] + 1 || 1;
+    // 3. Call set state to update the state object.
+    this.setState({ order });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map( key => 
+            <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)}          
+          </ul>
         </div>
         <Order />
            {/* Passing the addFish method to the Inventory component. */}
---
+++ src/components/Fish.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { formatPrice } from '../helpers';
+
+class Fish extends React.Component {
+  render() {
+
+    // ES6 destructuring. Sets each individual detail to it's own variable.
+    const {image, name, price, desc, status} = this.props.details;
+    const isAvailable = status === 'available';
+
+    return (
+    <li className="menu-fish">
+      <img src={image} alt={this.props.details.name}/>
+      <h3 className="fish-name">
+        {name}
+        <span className="price">{formatPrice(price)}</span>
+      </h3>
+      <p>{desc}</p>
+      <button onClick={() => this.props.addToOrder(this.props.index)} disabled={!isAvailable}>{isAvailable ? 'Add To Order' : 'SOLD OUT'}</button>
+    </li>
+    )
+  }
+}
+
+export default Fish;
